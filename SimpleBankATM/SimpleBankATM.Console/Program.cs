c9a0932002b6daf653983afb324da4f5dfb1f1d8
Ward <Jeff.Ward@umusic.com>
2016-10-22 21:42:06 -0700

    added global exception handling

--- SimpleBankATM/SimpleBankATM.Console/Program.cs
+++ SimpleBankATM/SimpleBankATM.Console/Program.cs
@@ -10,6 +10,18 @@ namespace SimpleBankATM.Console
     {
         static void Main(string[] args)
         {
+            //Global Exception Handler
+            System.AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+
+        }
+
+        static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            System.Console.WriteLine();
+            System.Console.WriteLine(e.ExceptionObject.ToString());
+            System.Console.WriteLine("Press Enter to continue");
+            System.Console.ReadLine();
+            Environment.Exit(1);
         }
     }
 }
