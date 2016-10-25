60d4b84d03163ddb7d55f386036bd430dc019f77
jeffward01 <jeffwardcoding@gmail.com>
2016-10-24 23:41:27 -0700

    unit-test busines requirements completed

--- SimpleBankATM/SimpleBankATM.Tests/AccountManagerTests.cs
+++ SimpleBankATM/SimpleBankATM.Tests/AccountManagerTests.cs
@@ -1,5 +1,6 @@
 ﻿using FakeItEasy;
 using NUnit.Framework;
+using SimpleBankATM.Business;
 using SimpleBankATM.Business.Managers;
 using SimpleBankATM.Data.Repositories;
 using SimpleBankATM.Models;
@@ -32,6 +33,104 @@ namespace SimpleBankATM.Tests
             // Assert
             Assert.AreEqual(3, accounts.Count(), "Should have 4");
             Assert.AreEqual(44, accounts.First().Balance, "Should be 44");
+        }
+
+        //Test user create account
+        [Test]
+        public void CreateAccount_UserCreateAccount()
+        {
+            var testData = new List<Account>
+            {
+                new Account {CustomerId = 1, Balance = 44, AccountTypeId = 1},
+             new Account {CustomerId = 1, Balance = 500, AccountTypeId = 1},
+            new Account {CustomerId = 1, Balance = 300, AccountTypeId = 2},
+            };
+
+            var context = A.Fake<IAccountRepository>();
+            A.CallTo(() => context.GetAllAccounts()).Returns(testData);
+
+            A.CallTo(() => context.CreateAccount(new Account { CustomerId = 1, Balance = 300, AccountTypeId = 2 })).Returns(new Account { CustomerId = 1, Balance = 300, AccountTypeId = 2 });
+
+            var accountManager = new AccountManager(context);
+            A.CallTo(() => context.GetAllAccounts()).Returns(testData);
+
+            // Act
+            var accounts = accountManager.AddAccount(1, AccountType.Checking);
+            var count = accountManager.GetAllAccounts();
+
+            // Assert
+            Assert.IsInstanceOf<Account>(accounts);
+        }
+
+        [Test]
+        public void DeleteAccount_UserDeleteAccount()
+        {
+            var context = A.Fake<IAccountRepository>();
+            A.CallTo(() => context.GetAccountById(1)).Returns(new Account { CustomerId = 1, AccountId = 1, Balance = 300, AccountTypeId = 2 });
+
+            A.CallTo(() => context.DeleteAccount(A<Account>.Ignored)).Returns(true);
+
+            var accountManager = new AccountManager(context);
+
+            // Act
+            var result = accountManager.DeleteAccount(1);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void UserCanWithdraw_UserCanWithdraw()
+        {
+            var context = A.Fake<ITransactionRepository>();
+            var handler = A.Fake<ITransactionHandler>();
+            var context1 = A.Fake<IAccountRepository>();
+
+            var transaction = new Transaction();
+            transaction.TransactionAmount = 15;
+            transaction.AccountId = 1;
+
+            TransactionStatus expenect = new TransactionStatus();
+            expenect.IsValid = true;
+
+            var transactionManager = new TransactionManager(context, handler);
+
+            A.CallTo(() => context1.GetAccountById(A<int>.Ignored)).Returns(new Account { CustomerId = 1, AccountId = 1, Balance = 3000, AccountTypeId = 2 });
+
+            A.CallTo(() => handler.IsValidTransaction(transaction, TransactionType.Withdrawl)).Returns(expenect);
+
+            // Act
+            var result = transactionManager.AddTransaction(15, 1, TransactionType.Withdrawl);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Test]
+        public void UserCanWithdraw_UserCanDeposit()
+        {
+            var context = A.Fake<ITransactionRepository>();
+            var handler = A.Fake<ITransactionHandler>();
+            var context1 = A.Fake<IAccountRepository>();
+
+            var transaction = new Transaction();
+            transaction.TransactionAmount = 15;
+            transaction.AccountId = 1;
+
+            TransactionStatus expenect = new TransactionStatus();
+            expenect.IsValid = true;
+
+            var transactionManager = new TransactionManager(context, handler);
+
+            A.CallTo(() => context1.GetAccountById(A<int>.Ignored)).Returns(new Account { CustomerId = 1, AccountId = 1, Balance = 3000, AccountTypeId = 2 });
+
+            A.CallTo(() => handler.IsValidTransaction(transaction, TransactionType.Deposit)).Returns(expenect);
+
+            // Act
+            var result = transactionManager.AddTransaction(15, 1, TransactionType.Deposit);
+
+            // Assert
+            Assert.False(result.IsValid);
         }
 
         [Test]
